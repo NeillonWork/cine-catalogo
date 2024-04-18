2df66a0b164a05e13ea7debd74926b6950937532
Admin <neillonwork@gmail.com>
2024-04-18 20:30:55 -0300

    criado a pagina de detalhes do filme

--- src/pages/Filme/filme.js
+++ src/pages/Filme/filme.js
@@ -1,9 +1,53 @@
-function Filme(){
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from "../../services/api";
+
+function Filme() {
+  const { id } = useParams();
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: "bd87ec7688f77cd16be1c6ecd91de7b2",
+            language: "pt-BR",
+          },
+        })
+        .then((response) => {
+          setFilme(response.data);
+          setLoading(false);
+        })
+        .catch(() => {
+          console.log("FILME NAO ENCONTRADO");
+        });
+    }
+    loadFilme();
+
+    return () => {
+      console.log("COMPONENTE FOI DESMONTADO");
+    };
+  }, []);
+
+  if (loading) {
     return (
-        <div>
-            <h1>Lista de Filmes</h1>
-        </div>
-    )
-};
+      <div className="filme-info">
+        <h1>Carregando detalhes do filme...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className="filme-info">
+        <h1>{filme.title}</h1>
+      <img  src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+      <strong>Avaliação: {filme.vote_average} /10</strong>
+    </div>
+  );
+}
 
 export default Filme;
